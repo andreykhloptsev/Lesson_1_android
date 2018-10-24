b7156d68bb4b02013bc63a2a1135662f7ca17dd3
andreykhloptsev <ahloptsev@gmail.com>
2018-10-24 15:29:20 +0300

    Lesson 2 Homework

--- desktop/src/ru/geekbrains/desktop/DesktopLauncher.java
+++ desktop/src/ru/geekbrains/desktop/DesktopLauncher.java
@@ -3,10 +3,11 @@ package ru.geekbrains.desktop;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import ru.geekbrains.StarGame;
+import ru.geekbrains.StarGame2D;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		new LwjglApplication(new StarGame(), config);
+		new LwjglApplication(new StarGame2D(), config);
 	}
 }
